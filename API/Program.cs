00701987c195e1cc3f976b19fcf9eecbca00126c
henryhaller99 <henryhernandezbarajas@gmail.com>
2021-02-28 15:12:36 -0600

    First commit

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using API.DataAccess;
+
+namespace API
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            NORTHWNDContext dbContext = new NORTHWNDContext(); // instanciamos el dbContext
+
+            var customers = dbContext.Customers.Select(c => new {
+                c.CustomerId,
+                c.ContactName,
+                c.Country
+            }).ToList(); // hacemos la consulta a la base de datos y la materializamos
+
+            customers.ForEach(x => Console.WriteLine("El cliente con el id " + x.CustomerId + " tiene nombre: " + x.ContactName + " y vive en " + x.Country ));
+        }
+    }
+}
