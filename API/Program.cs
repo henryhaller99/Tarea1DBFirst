e20922b8bdf3fa8af572f981c8685c191746f841
henryhaller99 <henryhernandezbarajas@gmail.com>
2021-03-20 12:24:21 -0600

    before changing Services/

--- API/Program.cs
+++ API/Program.cs
@@ -6,19 +6,35 @@ namespace API
 {
     class Program
     {
+        private static NORTHWNDContext dbContext = new NORTHWNDContext();
 
         static void Main(string[] args)
+        { 
+            PrintAllCustomers();
+            System.Console.WriteLine("-------------------------------------------------------------------");
+            PrintACustomer("TRAIH"); 
+        }
+
+        public static void PrintAllCustomers()
         {
-            Console.WriteLine("Hello World!");
-            NORTHWNDContext dbContext = new NORTHWNDContext(); // instanciamos el dbContext
-
-            var customers = dbContext.Customers.Select(c => new {
-                c.CustomerId,
-                c.ContactName,
-                c.Country
-            }).ToList(); // hacemos la consulta a la base de datos y la materializamos
-
+            var customers = GetCustomers().ToList(); 
             customers.ForEach(x => Console.WriteLine("El cliente con el id " + x.CustomerId + " tiene nombre: " + x.ContactName + " y vive en " + x.Country ));
+        }
+
+        public static void PrintACustomer(string id)
+        {
+            var customer = GetCustomerById(id);
+            System.Console.WriteLine("El cliente llamado(a): " + customer.ContactName + " vive en " + customer.Country);
+        }
+
+        private static IQueryable<Customer> GetCustomers()
+        {
+            return dbContext.Customers.Select(x => x);
+        }
+
+        private static Customer GetCustomerById(string id)
+        {
+            return GetCustomers().Where(x => x.CustomerId == id).FirstOrDefault();
         }
     }
 }
---
+++ API/Services/CustomerSC.cs
@@ -0,0 +1,10 @@
+using API.DataAccess;
+
+namespace API.Services
+{
+    
+    public class CustomerSC
+    {
+        NORTHWNDContext dbContext = new NORTHWNDContext();  
+    }
+}
